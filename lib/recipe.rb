5fd20022d39a5bdff907b829d39ed7e0f54162b6
Lauryn Davis and Riley Starnes <rstarnes9@gmail.com>
2016-01-28 10:12:24 -0800

    add validation and callback methods

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:tags)
+  validates(:recipe_name, :presence => true)
+    before_save(:titleize_recipe_name)
+
+  private
+
+  define_method(:titleize_recipe_name) do
+    self.recipe_name=(recipe_name().titleize())
+  end
 end
--- lib/tag.rb
+++ lib/tag.rb
@@ -1,3 +1,11 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many(:recipes)
+  validates(:category, :presence => true)
+  before_save(:titleize_category)
+
+  private
+
+  define_method(:titleize_category) do
+    self.category=(category().titleize())
+  end
 end
